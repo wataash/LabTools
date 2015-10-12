bf5ee959b9d7fc823f77e9dcad8b09637a30bcb0
wataash <expllion@gmail.com>
2015-10-12 20:23:55 +0900

    Dev

--- RenRHEED/MainWindow.xaml.cs
+++ RenRHEED/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace RenRHEED
 {
@@ -23,6 +24,74 @@ namespace RenRHEED
         public MainWindow()
         {
             InitializeComponent();
+            Watch();
+        }
+        FileSystemWatcher watcher = new FileSystemWatcher();
+        public void Watch()
+        {
+            watcher.Path = textBoxMoveFrom.Text;  // TODO change path text changed
+            watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite
+                      | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+            watcher.Filter = "*";
+
+            // Add event handlers.
+            watcher.Created += new FileSystemEventHandler(OnCreated);
+            watcher.Renamed += new RenamedEventHandler(OnRenamed);
+
+            // Begin watching.
+            watcher.EnableRaisingEvents = true;
+        }
+        private void OnCreated(object source, FileSystemEventArgs e)
+        {
+            string renFrom = textBoxRenameFrom.Text;
+            string renTo = textBoxRenameTo.Text;
+            string moveFrom = textBoxMoveFrom.Text;
+            string moveTo = textBoxMoveTo.Text;
+
+            string[] tokens = renFrom.Split(new[] { ' ' });
+            string fileName = e.Name;
+            string[] tokenValues = fileName.Split(new[] { ' ' });
+
+            if (tokens.Length != tokenValues.Length){
+                throw new ArgumentException(
+                    $"Rename from: {tokens.Length} given," +
+                    $"but filename: {fileName.Length}.");
+            }
+
+            for (int i = 0; i < tokens.Length; i++)
+            {
+                renFrom  = renFrom.Replace($"[{tokens[i]}]", tokenValues[i]);
+                renTo    = renTo.Replace($"[{tokens[i]}]", tokenValues[i]);
+                moveFrom = moveFrom.Replace($"[{tokens[i]}]", tokenValues[i]);
+                moveTo   = moveTo.Replace($"[{tokens[i]}]", tokenValues[i]);
+            }
+            Console.Write("");
+        }
+        private void OnRenamed(object source, RenamedEventArgs e)
+        {
+            OnCreated(source, (FileSystemEventArgs)e);
+        }
+        private void textBoxFileName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+        }
+        private void textBoxMoveFrom_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            //if (e.Key == Key.Return)
+            //{
+            //    watcher.Path = textBoxMoveFrom.Text;
+            //    MessageBox.Show(watcher.Path);
+
+            //}
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void buttonUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            watcher.Path = textBoxMoveFrom.Text;
         }
     }
 }
