acc081435e66a3fe1167387d9744d97653e1270b
wataash <expllion@gmail.com>
2015-11-27 17:50:53 +0900

    Dev

--- AFM/Export-CroppedImage.ps1
+++ AFM/Export-CroppedImage.ps1
@@ -1,2 +1,6 @@
-Get-ChildItem *.afm_3nm.jpg -Name |
-    %{convert $_ -crop 512x512+48+80 $("crop/" + $_)}
+Function Export-CroppedImage
+{
+    Get-ChildItem *.afm_3nm.jpg -Name |
+        %{convert $_ -crop 512x512+48+80 $("crop/{0}_crop.jpg" -f
+            [System.IO.Path]::GetFileNameWithoutExtension($_) ) }
+}
